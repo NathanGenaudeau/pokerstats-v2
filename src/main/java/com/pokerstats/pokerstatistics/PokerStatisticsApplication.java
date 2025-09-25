16b352382827a0b01b38871392e3843d9e6b59e1
Nathan Genaudeau <nathan.genaudeau@gmail.com>
2025-09-25 22:38:21 +0200

    init project

---
+++ src/main/java/com/pokerstats/pokerstatistics/PokerStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.pokerstats.pokerstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerStatisticsApplication.class, args);
+	}
+
+}
